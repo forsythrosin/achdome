179652221a9d7fe827500ff91e427d87e78fab18
Tomas Forsyth Rosin <tomas@forsythrosin.com>
2014-10-16 09:15:44 +0200

    Move copying webResources files to grunt.

--- src/webResources/Gruntfile.js
+++ src/webResources/Gruntfile.js
@@ -2,8 +2,14 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     watch: {
-      files: ['assets/js/modules/*.js', 'assets/templates/*.hbs', 'assets/templates/partials/*.hbs'],
-      tasks: ['browserify']
+      browserify: {
+        files: ['assets/js/modules/*.js', 'assets/templates/*.hbs', 'assets/templates/partials/*.hbs'],
+        tasks: ['browserify']
+      },
+      copy: {
+        files: ['*'],
+        tasks: ['copy']
+      }
     },
     browserify: {
       dist: {
@@ -14,9 +20,19 @@ module.exports = function(grunt) {
           transform: ['browserify-handlebars']
         }
       }
+    },
+    copy: {
+      main: {
+        src: ['./**/*', '!./**/node_modules/**'],
+        expand: true,
+        cwd: './',
+        dest: '../../build/webResources/'
+      }
     }
   });
 
+  grunt.registerTask('default', ['browserify']);
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-browserify');
 };
