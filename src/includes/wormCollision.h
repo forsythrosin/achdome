204548b51751b731270620fed0b42cef57f67692
emiax <mail@emilaxelsson.se>
2014-10-18 00:15:45 +0200

    add collision detection algorithm

--- src/includes/wormCollision.h
+++ src/includes/wormCollision.h
@@ -1,7 +1,11 @@
+#pragma once
+
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
 
 class WormCollision {
+ public:
+  WormCollision(int collider, int collidee, glm::quat position);
   int collider, collidee;
   glm::quat position;
 };
--- src/libs/wormArc.cpp
+++ src/libs/wormArc.cpp
@@ -13,21 +13,15 @@ int WormArc::getWormId() {
   return wormId;
 }
 
+int WormArc::getTime() {
+  return time;
+}
+
 glm::vec3 WormArc::getCartesianLerp(float t) {
-  // glm::vec3 v1 = glm::mat3_cast(glm::normalize(firstQuat)) * glm::vec3(1.0, 0.0, 0.0);
-  // glm::vec3 v2 = glm::mat3_cast(glm::normalize(secondQuat)) * glm::vec3(1.0, 0.0, 0.0);
-
-  // float angle = acos(glm::dot(v1, v2))*t;
-  // glm::vec3 rotationAxis = glm::cross(v1, v2);
-
-  // // Quaterion normalization
-  // rotationAxis *= sin(angle/2.0);
-  // float w = cos(angle/2.0);
-
-  // glm::quat interpolatedQuat(w, rotationAxis.x, rotationAxis.y, rotationAxis.z);
-
-  // return glm::mat3_cast(glm::normalize(interpolatedQuat)) * v1;
-
   glm::quat interpolatedQuat = glm::slerp(firstQuat, secondQuat, t);
   return glm::mat3_cast(glm::normalize(interpolatedQuat)) * glm::vec3(1.0, 0.0, 0.0);
+}
+
+glm::quat WormArc::getLerp(float t) {
+  return glm::slerp(firstQuat, secondQuat, t);
 }
